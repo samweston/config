e469121fb16e6c874370e68f3c2ab1c11147da47
Sam Weston <samueldweston@gmail.com>
2012-09-25 07:57:25 +0000

    removed lines from linker

--- linker.sh
+++ linker.sh
@@ -6,8 +6,8 @@ ln -s `pwd`/vimrc ~/.vimrc
 rm ~/.bashrc
 ln -s `pwd`/bashrc ~/.bashrc
 
-rm ~/.xinitrc
-ln -s `pwd`/xinitrc ~/.xinitrc
+#rm ~/.xinitrc
+#ln -s `pwd`/xinitrc ~/.xinitrc
 
-rm ~/.Xmodmap
-ln -s `pwd`/Xmodmap ~/.Xmodmap
+#rm ~/.Xmodmap
+#ln -s `pwd`/Xmodmap ~/.Xmodmap
