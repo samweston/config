dbca51ce453c7ffe2245bd60daf7c76f50255e25
sam <sam@localhost.localdomain>
2013-01-03 15:37:47 +1300

    go link

--- linker.sh
+++ linker.sh
@@ -3,7 +3,9 @@
 rm ~/.vimrc
 ln -s `pwd`/vimrc ~/.vimrc
 mkdir -p ~/.vim/syntax
-ln -s `pwd`/vim/syntax/armasm.vim ~/.vim/syntax/armasm.vim
+ln -s `pwd`/vim/syntax/* ~/.vim/syntax/ &> /dev/null
+mkdir -p ~/.vim/ftdetect
+ln -s `pwd`/vim/ftdetect/* ~/.vim/ftdetect/ &> /dev/null
 
 rm ~/.bashrc
 ln -s `pwd`/bashrc ~/.bashrc
